34de640a15a15ae200edc96e53f5d3be675b5ad8
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2025-02-05 15:02:28 +0530

    addd

--- frontend/src/services/serverURL.ts
+++ frontend/src/services/serverURL.ts
@@ -1,5 +1,7 @@
-export const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+// export const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 // export const AUTH_SERVICE_URL = import.meta.env.VITE_AUTH_SERVICE_URL;
+
+export const SERVER_URL = 'https://alter-office-machine-task-4.onrender.com';
 
 
 export const AUTH_SERVICE_URL = 'https://alter-office-machine-task-4.onrender.com';
